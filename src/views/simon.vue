47d22788af0e79b7dc0ddb3a0ffb66b6932a0be5
DConstantine9 <constantinedeniskkin@gmail.com>
2020-08-31 16:03:53 +0300

    final

--- src/views/simon.vue
+++ src/views/simon.vue
@@ -13,35 +13,38 @@
   <source src="../assets/4.mp3">
 </audio>
 
-  <label for="name">level </label>
-  <select v-model="level" name="level" id="level">
-    <option>Easy</option>
-    <option>Medium</option>
-    <option>Hard</option>
-  </select>
+  
 
-  <div id="outer-circle">
-    <div v-on:click="topleftFn" id="topleft"></div>
-    <div v-on:click="toprightFn" id="topright"></div>
-    <div v-on:click="bottomleftFn" id="bottomleft"></div>
-    <div v-on:click="bottomrightFn" id="bottomright"></div>
-    <div id="inner-circle">
-      <div id="title" class="font-effect-emboss">SIMON!</div>
-      <div id="switches">
-        <input v-on:change="onFn" type="checkbox" class="toggle" id="on">
-        <button v-on:click="startFn" class="button" id="start">Start</button>
-        <input v-on:change="strictFn" type="checkbox" class="toggle" id="strict">
-      </div>
-      <div class="text1">
-        <span>POWER</span><span>STRICT</span>
-      </div>
-      <div id="turn"></div>
-      <div class="text2">
-        COUNT
-      </div>
-    </div>
-  </div>
-</div>
+<div class="wrapper">
+		<h1>Simon Says</h1>
+			<div class="game-board">
+				<div class="simon">
+					<ul>
+						<li v-on:click="topleftFn" id="topleft" class="tile red" data-tile="1"></li>
+						<li v-on:click="toprightFn" id="topright" class="tile blue" data-tile="2"></li>
+						<li v-on:click="bottomleftFn" id="bottomleft" class="tile yellow" data-tile="3"></li>
+						<li v-on:click="bottomrightFn" id="bottomright" class="tile green" data-tile="4"></li>
+					</ul>
+				</div>
+			</div>
+			<div class="game-info">
+				<h2>Round: <span id="turn">0</span></h2>
+				<button v-on:click="startFn">Start</button>
+				<p data-action="lose">Sorry, you lost after <span data-round="0"></span> rounds!</p>
+			</div>
+      <label for="strict">Strict mode</label>
+			<input v-on:change="strictFn" type="checkbox" name="strict" id="strict">
+        <br>
+			<label for="name">level </label>
+      <select v-model="level" name="level" id="level">
+        <option>Easy</option>
+        <option>Medium</option>
+        <option>Hard</option>
+      </select>	
+	</div>
+
+
+</div> 
 </template>
 
 <script>
@@ -58,7 +61,7 @@ data: function() {
     intervalId: null,
     strict: false,
     noise: false,
-    on: false,
+    on: true,
     win: null,
     level: "Easy",
     levelInterval: 1500,
@@ -308,163 +311,140 @@ methods: {
 </script>
 
 <style lang="scss" scoped>
-.toggle {
-  margin-left: 20px;
-  margin-right: 20px;
+body {
+	font-family: Arial, serif;
+	color: #333;
+	-webkit-user-select: none; /* Chrome/Safari */        
+	-moz-user-select: none; /* Firefox */
+	-ms-user-select: none; /* IE10+ */
+	-o-user-select: none;
+	user-select: none;
 }
 
-.button {
-  border-radius: 50px !important;
-  font-size: 1.5em;
-  background-color: lightgray;
+h1 {
+	margin: 1em 0 2em;
+	text-align: center;
 }
 
-#switches {
-  position: absolute;
-  font-family: 'Roboto', sans-serif;
-  margin-left: 26px;
-  margin-top: 110px;
+ul {
+	list-style: none;
 }
 
-.text2 {
-  position: absolute;
-  font-family: 'Roboto', sans-serif;
-  margin-left: 83px;
-  margin-top: 197px;
+ul, li {
+	padding: 0;
+	margin: 0;
 }
 
-.text1 {
-  position: absolute;
-  font-family: 'Roboto', sans-serif;
-  margin-left: 19px;
-  margin-top: 142px;
+.active {
+	opacity: 1 !important;
 }
 
-.text1 span:last-child {
-  padding-left: 70px;
+.clearfix {
+	width: 100%;
+	clear: both;
 }
 
-#title {
-  font-family: 'Roboto', sans-serif;
-  position: absolute;
-  font-size: 40px;
-  margin-left: 35px;
-  margin-top: 40px;
+.wrapper {
+	width: 540px;
+	margin: 0 auto;
+}
+.container {
+	width: 305px;
 }
 
-#turn {
-  position: absolute;
-  background: #330000;
-  color: red;
-  font-family: courier;
-  font-size: 20px;
-  height: 30px;
-  width: 50px;
-  margin-left: 90px;
-  margin-top: 170px;
-  text-align: center;
-  vertical-align: middle;
-  line-height: 30px;
+.simon {
+	background: #fff;
+	position: relative;
+	float: left;
+	margin-right: 3em;	
+	width: 302px;
+	height: 295px;
+	-webkit-border-radius: 150px 150px 150px 150px;
+	border-radius: 150px 150px 150px 150px;
+    -moz-box-shadow: 2px 1px 12px #aaa;
+    -webkit-box-shadow: 2px 1px 12px #aaa;
+    -o-box-shadow: 2px 1px 12px #aaa;
+    box-shadow: 2px 1px 12px #aaa;
 }
 
-#outer-circle {
-  background: #385a94;
-  border-radius: 50%;
-  height: 500px;
-  width: 500px;
-  position: relative;
-  border-style: solid;
-  border-width: 10px;
-  margin: auto;
-  margin-top: 60px;
-  box-shadow: 8px 8px 15px 5px #888888;
+.tile {
+	opacity: 0.6;
+	-webkit-transition: opacity 250ms ease;
+	-moz-transition: opacity 250ms ease;
+	-ms-transition: opacity 250ms ease;
+	-o-transition: opacity 250ms ease;
+	transition: opacity 250ms ease;
 }
 
-#topleft {
-  position: absolute;
-  height: 250px;
-  width: 250px;
-  border-radius: 250px 0 0 0;
-  -moz-border-radius: 250px 0 0 0;
-  -webkit-border-radius: 250px 0 0 0;
-  background: darkgreen;
-  top: 50%;
-  left: 50%;
-  margin: -250px 0px 0px -250px;
-  border-style: solid;
-  border-width: 5px;
-  box-sizing: border-box;
-  -moz-box-sizing: border-box;
-  -webkit-box-sizing: border-box;
+.tile.lit {
+	opacity: 1;
 }
 
-#topright {
-  position: absolute;
-  height: 250px;
-  width: 250px;
-  border-radius: 0 250px 0 0;
-  -moz-border-radius: 0 250px 0 0;
-  -webkit-border-radius: 0 250px 0 0;
-  background: darkred;
-  top: 50%;
-  left: 50%;
-  margin: -250px 0px 0px 0px;
-  border-style: solid;
-  border-width: 5px;
-  box-sizing: border-box;
-  -moz-box-sizing: border-box;
-  -webkit-box-sizing: border-box;
+.red, .blue, .yellow, .green {
+	height: 290px;
+	-webkit-border-radius: 150px 150px 150px 150px;
+	border-radius: 150px 150px 150px 150px;
+	position: absolute;
+	text-indent: 10000px;
 }
 
-#bottomleft {
-  position: absolute;
-  height: 250px;
-  width: 250px;
-  border-radius: 0 0 0 250px;
-  -moz-border-radius: 0 0 0 250px;
-  -webkit-border-radius: 0 0 0 250px;
-  background: goldenrod;
-  top: 50%;
-  left: 50%;
-  margin: 0px -250px 0px -250px;
-  border-style: solid;
-  border-width: 5px;
-  box-sizing: border-box;
-  -moz-box-sizing: border-box;
-  -webkit-box-sizing: border-box;
+.red:hover, .blue:hover, .yellow:hover, .green:hover {
+	border: 2px solid black
 }
 
-#bottomright {
-  position: absolute;
-  height: 250px;
-  width: 250px;
-  border-radius: 0 0 250px 0;
-  -moz-border-radius: 0 0 250px 0;
-  -webkit-border-radius: 0 0 250px 0;
-  background: darkblue;
-  top: 50%;
-  left: 50%;
-  margin: 0px 0px -250px 0px;
-  border-style: solid;
-  border-width: 5px;
-  box-sizing: border-box;
-  -moz-box-sizing: border-box;
-  -webkit-box-sizing: border-box;
+.red {
+	background: #FF5643;
+	clip: rect(0px, 300px, 150px, 150px);
+	width: 296px;
 }
 
-#inner-circle {
-  position: absolute;
-  background: darkgray;
-  border-radius: 50%;
-  height: 250px;
-  width: 250px;
-  border-style: solid;
-  border-width: 10px;
-  top: 50%;
-  left: 50%;
-  margin: -125px 0px 0px -125px;
-  box-sizing: border-box;
-  -moz-box-sizing: border-box;
-  -webkit-box-sizing: border-box;
+.blue {
+	background: dodgerblue;
+	clip: rect(0px, 150px, 150px, 0px);
+	width: 300px;
+}
+
+.yellow {
+	background: #FEEF33;
+	clip: rect(150px, 150px, 300px, 0px);
+	width: 300px;
+}
+
+.green {
+	background: #BEDE15;
+	clip: rect(150px,300px, 300px, 150px);
+	width: 296px;
+}
+
+.game-info {
+	margin-top: 90px;
+}
+
+.game-info button {
+	width: 5em;
+	box-sizing: border-box;
+	font-size: 1.4em;
+	-webkit-border-radius: 10px 10px 10px 10px;
+	border-radius: 10px 10px 10px 10px;
+	background: #6DABE8;
+	border: none;
+	padding: 0.3em 0.6em;
+}
+
+.game-info button:hover {
+	background: #78BCFF;
+}
+
+.game-options h2 {
+	margin-top: 30px;
+	margin-bottom: 0;
+}
+
+.game-options input[type="radio"] {
+	margin-right: 10px;
+}
+
+.hoverable:hover {
+	cursor: pointer;
 }
 </style>
